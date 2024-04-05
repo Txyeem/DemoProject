546c3c3734252a4d71569c6223c09808f66526da
Tayeem Rahman <Txyeem@gmail.com>
2024-04-05 14:43:46 +0100

    Home

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,25 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'gilroy': ['Gilroy', 'sans-serif'],
+        'gilroy-bold': ['Gilroy-Bold', 'sans-serif'],
+        'gilroy-heavy': ['Gilroy-Heavy', 'sans-serif'],
+        'gilroy-light': ['Gilroy-Light', 'sans-serif'],
+        'gilroy-medium': ['Gilroy-Medium', 'sans-serif'],
+        'gilroy-regular': ['Gilroy-Regular', 'sans-serif'],
+        'poppins': ["Poppins", 'sans-serif']
       },
-    },
+      fontWeight: {
+        'thin': 100,
+        'extralight': 200,
+        'light': 300,
+        'regular': 400,
+        'medium': 500,
+        'semibold': 600,
+        'bold': 700,
+        'extrabold': 800,
+        'black': 900,
+      },
+    }, // Close extend
   },
   plugins: [],
 };
