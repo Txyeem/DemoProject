526169af14cbfa3d7c4c5d86801da8305f277b1c
Tayeem Rahman <Txyeem@gmail.com>
2024-04-05 16:58:15 +0100

    Features

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,10 @@ export default function Home() {
     <div className="text-black pt-28">
       <div className="container mx-auto text-center">
         <h1 className="text-[55px] font-poppins font-semibold mb-4">Pricing & Plans</h1>
-        <p className="text-[18px] font-poppins font-extralight mx-[310px] pt-8 text-[#161C2D] opacity-90">With lots of unique blocks, you can easily build a page without coding. Build your next consultancy website within a few minutes.</p>
+        <p className="text-[18px] font-poppins font-extralight mx-[310px] pt-8 text-[#161C2D] opacity-90">
+          With lots of unique blocks, you can easily build a page without coding.<br/>
+        Build your next consultancy website within a few minutes.
+        </p>
       </div>
     </div>
     
--- src/components/info.jsx
+++ src/components/info.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
+// Features
+
 function Info() {
   return (
-    <section>
-      Info
-    </section>
+    <div className="text-black pt-28">
+      <div className="container mx-auto text-center">
+        <h1 className="text-[55px] font-poppins font-semibold mb-2">Check our features</h1>
+        <p className="text-[18px] font-poppins font-extralight mx-[350px] pt-8 text-[#161C2D] opacity-90">
+            With lots of unique blocks, you can easily build a page without coding.<br/>
+            Build your next consultancy website within few minutes.
+        </p>
+      </div>
+    </div>
   );
 }
 
