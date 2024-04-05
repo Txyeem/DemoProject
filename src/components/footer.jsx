4d698157c4f1da13cfccb7c09be1d2c025406022
Tayeem Rahman <Txyeem@gmail.com>
2024-04-05 18:57:17 +0100

    footer wip

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,9 +2,50 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer>
-      Footer 
-    </footer>
+<div className='bg-[#161C2D] p-10'>
+      <div className="grid grid-cols-5 gap-4 mx-[300px] text-center">
+        <div className='col-span-1'>
+          <h1 className='text-white'>Column 1</h1>
+          <p className="text-white">Hello</p>
+          <p className="text-white">Hello</p>
+          <p className="text-white">Hello</p>
+          <p className="text-white">Hello</p>
+          {/* Add more rows of text for column 1 here */}
+        </div>
+        <div className='col-span-1'>
+          <h1 className='text-white'>Column 2</h1>
+          <p className="text-white">Hello</p>
+          <p className="text-white">Hello</p>
+          <p className="text-white">Hello</p>
+          <p className="text-white">Hello</p>
+          {/* Add more rows of text for column 2 here */}
+        </div>
+        <div className='col-span-1'>
+          <h1 className='text-white'>Column 3</h1>
+          <p className="text-white">Hello</p>
+          <p className="text-white">Hello</p>
+          <p className="text-white">Hello</p>
+          <p className="text-white">Hello</p>
+          {/* Add more rows of text for column 3 here */}
+        </div>
+        <div className='col-span-1'>
+          <h1 className='text-white'>Column 4</h1>
+          <p className="text-white">Hello</p>
+          <p className="text-white">Hello</p>
+          <p className="text-white">Hello</p>
+          <p className="text-white">Hello</p>
+          {/* Add more rows of text for column 4 here */}
+        </div>
+        <div className='col-span-1'>
+          <h1 className='text-white'>Column 5</h1>
+          <p className="text-white">Hello</p>
+          <p className="text-white">Hello</p>
+          <p className="text-white">Hello</p>
+          <p className="text-white">Hello</p>
+          {/* Add more rows of text for column 5 here */}
+        </div>
+      </div>
+    </div>
   );
 }
 
