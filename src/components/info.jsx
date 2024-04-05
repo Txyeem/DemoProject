19b7648c26aeb0a79a72234ed0ffa8259f9c928e
Tayeem Rahman <Txyeem@gmail.com>
2024-04-05 18:04:46 +0100

    Features

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -11,8 +11,97 @@ function Info() {
             With lots of unique blocks, you can easily build a page without coding.<br/>
             Build your next consultancy website within few minutes.
         </p>
+    
+      <div className="grid grid-cols-3 gap-10 mt-12 max-w-[1100px] mx-auto pt-10 text-left pb-40">
+
+        {/* Top row */}
+        <div className="p-2 relative">
+          <div>
+            
+          <div className="absolute top-0 left-0 p-3 -10">
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#000000" viewBox="0 0 256 256">
+                  <path d="M40,88H73a32,32,0,0,0,62,0h81a8,8,0,0,0,0-16H135a32,32,0,0,0-62,0H40a8,8,0,0,0,0,16Zm64-24A16,16,0,1,1,88,80,16,16,0,0,1,104,64ZM216,168H199a32,32,0,0,0-62,0H40a8,8,0,0,0,0,16h97a32,32,0,0,0,62,0h17a8,8,0,0,0,0-16Zm-48,24a16,16,0,1,1,16-16A16,16,0,0,1,168,192Z"></path>
+              </svg>
+          </div>
+
+              <h2 className="font-poppins font-semibold text-[21px] pb-5 pl-14">Quick setup process</h2>
+              <p className="text-[17px] font-poppins font-extralight pl-12">With lots of unique blocks, you can easily build a page without coding. Build your next landing page.</p>
+          </div>
+        </div>
+
+        {/* 1 */}
+
+          <div className="p-2 relative">
+
+          <div className="absolute top-0 left-0 p-3 -10">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#000000" viewBox="0 0 256 256">
+              <path d="M230.14,58.87A8,8,0,0,0,224,56H62.68L56.6,22.57A8,8,0,0,0,48.73,16H24a8,8,0,0,0,0,16h18L67.56,172.29a24,24,0,0,0,5.33,11.27,28,28,0,1,0,44.4,8.44h45.42A27.75,27.75,0,0,0,160,204a28,28,0,1,0,28-28H91.17a8,8,0,0,1-7.87-6.57L80.13,152h116a24,24,0,0,0,23.61-19.71l12.16-66.86A8,8,0,0,0,230.14,58.87ZM104,204a12,12,0,1,1-12-12A12,12,0,0,1,104,204Zm96,0a12,12,0,1,1-12-12A12,12,0,0,1,200,204Zm4-74.57A8,8,0,0,1,196.1,136H77.22L65.59,72H214.41Z"></path>
+            </svg>
+          </div>
+
+              <h2 className="font-poppins font-semibold text-[21px] pb-5 pl-14">Create custom orders</h2>
+              <p className="text-[17px] font-poppins font-extralight pl-12">With lots of unique blocks, you can easily build a page without coding. Build your next landing page.</p>
+          </div>
+
+        {/* 2 */}
+
+          <div className="p-2 relative">
+
+          <div className="absolute top-0 left-0 p-3 -10">
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#000000" viewBox="0 0 256 256">
+            <path d="M232,104a8,8,0,0,0,8-8V64a16,16,0,0,0-16-16H32A16,16,0,0,0,16,64V96a8,8,0,0,0,8,8,24,24,0,0,1,0,48,8,8,0,0,0-8,8v32a16,16,0,0,0,16,16H224a16,16,0,0,0,16-16V160a8,8,0,0,0-8-8,24,24,0,0,1,0-48ZM32,167.2a40,40,0,0,0,0-78.4V64H88V192H32Zm192,0V192H104V64H224V88.8a40,40,0,0,0,0,78.4Z"></path>
+            </svg>
+          </div>
+
+              <h2 className="font-poppins font-semibold text-[21px] pb-5 pl-14">Multiple coupons</h2>
+              <p className="text-[17px] font-poppins font-extralight pl-12">With lots of unique blocks, you can easily build a page without coding. Build your next landing page.</p>
+          </div>
+
+        {/* 3 */}
+
+          {/* Bottom row */}
+          <div className="p-2 relative">
+
+          <div className="absolute top-0 left-0 p-3 -10">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256">
+            <path d="M12,111l112,64a8,8,0,0,0,7.94,0l112-64a8,8,0,0,0,0-13.9l-112-64a8,8,0,0,0-7.94,0l-112,64A8,8,0,0,0,12,111ZM128,49.21,223.87,104,128,158.79,32.13,104ZM246.94,140A8,8,0,0,1,244,151L132,215a8,8,0,0,1-7.94,0L12,151A8,8,0,0,1,20,137.05l108,61.74,108-61.74A8,8,0,0,1,246.94,140Z"></path>
+            </svg>
+          </div>
+
+              <h2 className="font-poppins font-semibold text-[21px] pt-1 pb-5 pl-14">Organize your campaigns</h2>
+              <p className="text-[17px] font-poppins font-extralight pl-12">With lots of unique blocks, you can easily build a page without coding. Build your next landing page.</p>
+          </div>
+
+          <div className="p-2 relative">
+
+          <div className="absolute top-0 left-0 p-3 -10">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256">
+            <path d="M144,157.68a68,68,0,1,0-71.9,0c-20.65,6.76-39.23,19.39-54.17,37.17a8,8,0,1,0,12.24,10.3C50.25,181.19,77.91,168,108,168s57.75,13.19,77.87,37.15a8,8,0,0,0,12.26-10.3C183.18,177.07,164.6,164.44,144,157.68ZM56,100a52,52,0,1,1,52,52A52.06,52.06,0,0,1,56,100Zm196.25,43.07-4.66-2.69a23.6,23.6,0,0,0,0-8.76l4.66-2.69a8,8,0,1,0-8-13.86l-4.67,2.7a23.92,23.92,0,0,0-7.58-4.39V108a8,8,0,0,0-16,0v5.38a23.92,23.92,0,0,0-7.58,4.39l-4.67-2.7a8,8,0,1,0-8,13.86l4.66,2.69a23.6,23.6,0,0,0,0,8.76l-4.66,2.69a8,8,0,0,0,8,13.86l4.67-2.7a23.92,23.92,0,0,0,7.58,4.39V164a8,8,0,0,0,16,0v-5.38a23.92,23.92,0,0,0,7.58-4.39l4.67,2.7a7.92,7.92,0,0,0,4,1.07,8,8,0,0,0,4-14.93ZM216,136a8,8,0,1,1,8,8A8,8,0,0,1,216,136Z"></path>
+            </svg>
+          </div>
+
+              <h2 className="font-poppins font-semibold text-[21px] pt-1 pb-5 pl-14">Manage customers</h2>
+              <p className="text-[17px] font-poppins font-extralight pl-12">With lots of unique blocks, you can easily build a page without coding. Build your next landing page.</p>
+          </div>
+
+          <div className="p-2 relative">
+
+          <div className="absolute top-0 left-0 p-3 -10">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M224,200h-8V40a8,8,0,0,0-8-8H152a8,8,0,0,0-8,8V80H96a8,8,0,0,0-8,8v40H48a8,8,0,0,0-8,8v64H32a8,8,0,0,0,0,16H224a8,8,0,0,0,0-16ZM160,48h40V200H160ZM104,96h40V200H104ZM56,144H88v56H56Z">
+            </path>
+          </svg>
+          </div>
+
+              <h2 className="font-poppins font-semibold text-[21px] pt-1 pb-5 pl-14">Track progress fast</h2>
+              <p className="text-[17px] font-poppins font-extralight pl-12">With lots of unique blocks, you can easily build a page without coding. Build your next landing page.</p>
+          </div>
+
+        </div>
+
       </div>
     </div>
+
+
   );
 }
 
